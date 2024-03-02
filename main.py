16f84f410514aa926827db868f350b79762b5705
xlop4ikk <bolshakovdan621@mail.ru>
2024-03-02 14:39:56 +0500

    asd

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 # -------------------------------------------------------------------------
-# Парсер товаров с Ozon
+# Парсер товаров с Optogadzhet
 # -------------------------------------------------------------------------
 
-from bs4 import BeautifulSoup as BS
 import requests
 import csv
+from bs4 import BeautifulSoup as BS
 from tkinter import *
-import random
 
-lst_titles = []
-lst_links = []
-lst_prices = []
-absolute_links = []
 headers = ['Наименование', 'Ссылка', 'Цена']
 
 root = Tk()
@@ -19,28 +14,52 @@ root.title("OptogadzhetParser")
 root.geometry("200x200")
 root.resizable(False, False)
 root.configure(bg="white")
-success = Label(root, text="Парсинг прошел успешно!")
-error = Label(root, text="Такой страницы нет!")
+label = Label(root, text="Введите номер страницы:", bg="white", fg="black", font=("Consolas", 11))
+texbox = Entry(root, width=25)
+
+def open_success_form():
+    success_window = Toplevel(root)
+    success_window.title("Успех")
+    success_label = Label(success_window, text="Программа выполнена успешно!")
+    success_label.pack()
+
+def open_error_form():
+    error_window = Toplevel(root)
+    error_window.title("Ошибка")
+    error_label = Label(error_window, text="Произошла ошибка в программе.")
+    error_label.pack()
 
 def parsing():
-    url = 'https://optogadzhet.ru/product-category/apple/iphone/'
+    lst_titles = []
+    lst_links = []
+    lst_prices = []
+    try:
+        number_of_page = int(texbox.get())
+        open_success_form()
+    except:
+        open_error_form()
+    url = f'https://optogadzhet.ru/product-category/apple/iphone/?page={number_of_page}'
     response = requests.get(url)
     html_content = response.text
     soup = BS(html_content, 'html.parser')
     
-    #print('Названия:')
+    # -------------------------------------------------------------------------
+    # Парсинг наименований товаров
+    # -------------------------------------------------------------------------
     titles = soup.find_all('h3', class_='card-item__title')
     for title in titles:
         lst_titles.append(title.text.strip())
-    #print(lst_titles)
 
-    #print('Ссылки:')
+    # -------------------------------------------------------------------------
+    # Парсинг ссылок на товары
+    # -------------------------------------------------------------------------
     links = soup.find_all('a', class_='card-item__link')
     for link in links:
         lst_links.append(link.get('href'))
-    #print(lst_links)
-    
-    #print('Цены:')
+
+    # -------------------------------------------------------------------------
+    # Парсинг цен на товары
+    # -------------------------------------------------------------------------
     prices = soup.find_all('span', class_='woocommerce-Price-amount amount')
     for price in prices:
         lst_prices.append(price.text.strip())
@@ -48,22 +67,16 @@ def parsing():
     final_list = [item.replace(' ', '') for item in modified_list]
     lst_prices_final = [int(item) for item in final_list] 
     lst_prices_final.pop(0)
-    #print(lst_prices_final)
     
-    #print(len(lst_titles))
-    #print(len(lst_links))
-    #print(len(lst_prices_final))
-    
-    """random_number = random.randint(0, 100)
-    with open(f'output{random_number}.csv', 'w', newline='', encoding="utf-8") as file:
+    # -------------------------------------------------------------------------
+    # Создание .csv файла и последующая запись данных
+    # -------------------------------------------------------------------------
+    with open(f'output{number_of_page}.csv', 'w', newline='', encoding="utf-8") as file:
         writer = csv.writer(file, delimiter=';')
         writer.writerow(headers)
         for data in zip(lst_titles, lst_links, lst_prices_final):
-            writer.writerow(data)"""
+            writer.writerow(data)
 
-#parsing()
-label = Label(root, text="Введите номер страницы:", bg="white", fg="black", font=("Consolas", 11))
-texbox = Entry(root, width=25)
 button = Button(root, text="Начать парсинг!", width=20, height=3, command=parsing)
 label.pack(ipadx=20, ipady=8, pady=5, side= "top")
 texbox.pack(ipadx=20, ipady=0, pady=5, side= "top")
